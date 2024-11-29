fd02e845d722ced4080d4ad4bc9dbb420c80521f
Ludmila <ludagrigoryeva@gmail.com>
2024-11-29 16:56:10 +0300

    испр файлы 2_4 и 2_6

--- module_2/module_2_6.py
+++ module_2/module_2_6.py
@@ -1,9 +1,7 @@
 # Дополнительное практическое задание по модулю: "Основные операторы"
 # Задание "Слишком древний шифр":
 
-import random
-
-n = 5
+n = 15
 
 print("Число из первой вставки: ", n)
 numbers = []
@@ -12,8 +10,6 @@ for i in range(1, n):
     for j in range(i + 1, n):
         if n % (i + j) == 0:
             numbers.append(str(i) + str(j))
-
-print(numbers)
 
 password = ''.join(numbers)
 
