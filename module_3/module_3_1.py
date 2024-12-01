97dbb855d99a314536db1c047bfb44e79dc3da8f
Ludmila <ludagrigoryeva@gmail.com>
2024-12-02 01:13:10 +0300

    ДЗ к уроку "Пространство имён" 02.12.24

---
+++ module_3/module_3_1.py
@@ -0,0 +1,45 @@
+# урок "Пространство имён"
+print('Задача "Счётчик вызовов": ')
+
+calls = 0
+def count_calls():
+    global calls
+    calls = calls + 1
+
+def string_info(string):
+    count_calls()
+    s_info = (len(string), string.upper(), string.lower())
+    return(s_info)
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for elem in list_to_search:
+        new_list = elem.lower()
+
+    if string.lower() in new_list:
+        return(True)
+    else:
+        return(False)
+    count_calls()
+
+print(string_info('Capybara'))
+
+print(string_info('Armageddon'))
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+
+print(calls)
+# #
+# Вывод на консоль:
+# (8, 'CAPYBARA', 'capybara')
+#(10, 'ARMAGEDDON', 'armageddon')
+#True
+# False
+# 4
+#
+# Примечания:
+# Для использования глобальной переменной внутри функции используйте оператор global.
+# Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.
+
