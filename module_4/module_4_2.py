2a896be5ba365c1ec4e470fb3efd1be3b0b19713
Ludmila <ludagrigoryeva@gmail.com>
2024-12-12 11:56:26 +0300

    Домашняя работа по уроку "Пространство имен." Задача " Создайте новую функцию test_function Создайте внутри test_function другую функцию - inner_function, Эта функция должна печатать значение "Я в области видимости функции test_function" Вызовите функцию inner_function внутри функции test_function Попробуйте вызывать inner_function вне функции test_function и посмотрите на результат выполнения программы 12.12.24

--- module_4/module_4_2.py
+++ module_4/module_4_2.py
@@ -1,9 +1,10 @@
 def test_function():
     def inner_function():
-        return "Я в области видимости функции test_function"
+        print("Я в области видимости функции test_function")
 
-    print(inner_function())
+    return inner_function()
 
+test_function()
 
 # inner_function()
 # при вызове inner_function вне функции test_function возникает ошибка:
