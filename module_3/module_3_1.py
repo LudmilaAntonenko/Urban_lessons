c5618696253cb40123abf8d6714a545c3b210baf
Ludmila <ludagrigoryeva@gmail.com>
2024-12-13 16:30:26 +0300

    Домашняя работа по уроку "Пространство имен." Задача "Счётчик вызовов":
    Ответ на комментарии от 02.12.24
    
    13.12.24

--- module_3/module_3_1.py
+++ module_3/module_3_1.py
@@ -1,33 +1,29 @@
-# урок "Пространство имён"
-print('Задача "Счётчик вызовов": ')
-
-calls = 0
 def count_calls():
     global calls
     calls = calls + 1
 
+
 def string_info(string):
     count_calls()
     s_info = (len(string), string.upper(), string.lower())
-    return(s_info)
+    return s_info
+
 
 def is_contains(string, list_to_search):
     count_calls()
     for elem in list_to_search:
-        new_list = elem.lower()
+        if string.lower() in elem.lower():
+            return True
 
-    if string.lower() in new_list:
-        return(True)
-    else:
-        return(False)
-    count_calls()
+    return False
+
+
+calls = 0
 
 print(string_info('Capybara'))
-
 print(string_info('Armageddon'))
 
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 
 print(calls)
