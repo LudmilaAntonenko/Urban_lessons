b2d1b73d6a400a0db8d6f7e060af63a10b36d15f
Ludmila <ludagrigoryeva@gmail.com>
2025-01-23 14:07:15 +0300

    Домашняя работа по уроку "Перегрузка операторов"
    Задача "Нужно больше этажей":
    
    Для решения этой задачи будем пользоваться решением к предыдущей задаче "Специальные методы класса".
    
    Необходимо дополнить класс House следующими специальными методами:
    
    __eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
    Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
    __add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
    __radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
    Остальные методы арифметических операторов, где self - x, other - y:
    
    23.01.25

--- module_5/module_5_4.py
+++ module_5/module_5_4.py
@@ -1,4 +1,9 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return cls.houses_history
 
     def __init__(self, name, number_of_floors):
         self.name = name
@@ -11,8 +16,7 @@ class House:
         return str(f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
 
     def __eq__(self, other):
-        if isinstance(other.number_of_floors, int) and isinstance(other, House):
-            return self.number_of_floors == other.number_of_floors
+        return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
         return self.number_of_floors < other.number_of_floors
@@ -30,29 +34,23 @@ class House:
         return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
-        if isinstance(value, int):
-            return House(self.name, self.number_of_floors + value)
+        return House(self.name, self.number_of_floors + value)
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он останется в истории')
+
 
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1 == h2) # __eq__
-
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-
-h1 += 10 # __iadd__
-print(h1)
-
-h2 = h2 + 10 # __radd__
-print(h2)
-
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+# Удаление объектов
+del h2
+# print(House.houses_history)
+del h3
+# print(House.houses_history)
